688e01ce7feb92b8c0b5c166fe69b94cab545a44
TheProgDave <58495011+TheProgDave@users.noreply.github.com>
2023-06-20 16:01:08 +1200

    Update streamlit_app.py
    typo again

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,4 @@ sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-sl.datframe(my_fruit_list)
+sl.dataframe(my_fruit_list)
